f4c82169491721d2b4700a095358892ac34457f2
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-10 14:57:00 -0700

    Initial Commit

---
+++ iOS_Gestures_Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_Gestures_Example
+//
+//  Created by Nathan Wainwright on 2018-08-10.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS_Gestures_ExampleTests/iOS_Gestures_ExampleTests.m
@@ -0,0 +1,39 @@
+//
+//  iOS_Gestures_ExampleTests.m
+//  iOS_Gestures_ExampleTests
+//
+//  Created by Nathan Wainwright on 2018-08-10.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS_Gestures_ExampleTests : XCTestCase
+
+@end
+
+@implementation iOS_Gestures_ExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
