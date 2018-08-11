222ea9d7d1cfe215afa9396f19772bb316865d3f
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-10 18:41:19 -0700

    setup PinchGesture.

---
+++ iOS_Gestures_Example/PinchViewController.m
@@ -0,0 +1,50 @@
+//
+//  PinchViewController.m
+//  iOS_Gestures_Example
+//
+//  Created by Nathan Wainwright on 2018-08-10.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#import "PinchViewController.h"
+
+@interface PinchViewController ()
+
+@end
+
+@implementation PinchViewController
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    CGFloat width = 100;
+    CGFloat height = 100;
+    CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) - width/2, CGRectGetMidY(self.view.bounds) - height/2, width, height);
+    
+    UIView *view = [[UIView alloc] initWithFrame:frame];
+    
+    view.backgroundColor = [UIColor purpleColor];
+    
+    [self.view addSubview:view];
+    
+    UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(viewPinched:)];
+    [view addGestureRecognizer:pinchGesture];
+    
+    
+}
+
+
+
+
+- (void) viewPinched:(UIPinchGestureRecognizer *) sender{
+    CGFloat scale = sender.scale;
+    sender.view.transform = CGAffineTransformMakeScale(scale, scale);
+    scale = 1.0;
+}
+
+
+
+
+
+
+
+@end
